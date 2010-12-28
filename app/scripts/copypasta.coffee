2404f3c92d28c3f6e6f6daaaac6213741dcc110e
mrkurt <kurt@mubble.net>
2010-12-28 15:10:22 -0600

    Making frames talk back to parent

--- app/scripts/copypasta.coffee
+++ app/scripts/copypasta.coffee
@@ -1,11 +1,20 @@
 $ = jQuery
 currentLive = false
 
+window.copypasta = copypasta =
+  init_frame : (parent_url) ->
+    copypasta.parent_url = parent_url
+    $('.close').live 'click', ()-> copypasta.postMessage('finished')
+  postMessage : (msg) ->
+    t = parent || window
+    t.postMessage(msg, copypasta.parent_url)
+
 ids =
   indicator: 'copy-pasta-edit-indicator'
   dialog: 'copy-pasta-dialog'
   iframe : 'copy-pasta-iframe'
   form : 'copy-pasta-form'
+  cancel_btn : 'copy-pasta-cancel'
 
 paths =
   indicator: '#' + ids.indicator
@@ -13,10 +22,11 @@ paths =
   btn: '.copy-pasta-button'
   active: '.copy-pasta-active'
   form: '#' + ids.form
+  cancel_btn: '#' + ids.cancel_btn
 
 default_form = '<form style="" id="' + ids.form + '" method="post" action="http://localhost:3000/edits/new" target="' + ids.iframe + '"><input type="hidden" name="view" value="framed"></form>'
 
-blank_dialog = '<div id="' + ids.dialog + '"><iframe frameborder="no" style="margin: 0px; padding: 0px; width: 400px; height: 400px;" id="' + ids.iframe + '" scrolling="no"></iframe></div>'
+blank_dialog = '<div id="' + ids.dialog + '"><iframe frameborder="no" style="margin: 0px; padding: 0px; width: 400px; height: 400px;" id="' + ids.iframe + '" scrolling="no"></iframe><input type="button" class="close" id="copy-pasta-cancel" style="display:none;"></div>'
 
 indicator = () ->
   if $(paths.indicator).length == 0
@@ -66,7 +76,10 @@ show_widget = () ->
   $(paths.form).submit()
 
 iframe_action = (e) ->
-  console.debug(e)
+  return unless e.origin = 'http://localhost:3000'
+  if e.data == 'finished'
+    $(paths.btn + '.on').click()
+    dialog().find(paths.cancel_btn).click()
 
 watch el for el in ['p', 'li', 'h1', 'h2', 'h3', 'h4', 'h5']
 
--- public/javascripts/copypasta.js
+++ public/javascripts/copypasta.js
@@ -1,23 +1,38 @@
 (function() {
-  var $, activate, blank_dialog, currentLive, deactivate, default_form, dialog, el, ids, iframe_action, indicator, paths, show_widget, watch, _i, _len, _ref;
+  var $, activate, blank_dialog, copypasta, currentLive, deactivate, default_form, dialog, el, ids, iframe_action, indicator, paths, show_widget, watch, _i, _len, _ref;
   var __hasProp = Object.prototype.hasOwnProperty;
   $ = jQuery;
   currentLive = false;
+  window.copypasta = copypasta = {
+    init_frame: function(parent_url) {
+      copypasta.parent_url = parent_url;
+      return $('.close').live('click', function() {
+        return copypasta.postMessage('finished');
+      });
+    },
+    postMessage: function(msg) {
+      var t;
+      t = parent || window;
+      return t.postMessage(msg, copypasta.parent_url);
+    }
+  };
   ids = {
     indicator: 'copy-pasta-edit-indicator',
     dialog: 'copy-pasta-dialog',
     iframe: 'copy-pasta-iframe',
-    form: 'copy-pasta-form'
+    form: 'copy-pasta-form',
+    cancel_btn: 'copy-pasta-cancel'
   };
   paths = {
     indicator: '#' + ids.indicator,
     dialog: '#' + ids.dialog,
     btn: '.copy-pasta-button',
     active: '.copy-pasta-active',
-    form: '#' + ids.form
+    form: '#' + ids.form,
+    cancel_btn: '#' + ids.cancel_btn
   };
   default_form = '<form style="" id="' + ids.form + '" method="post" action="http://copypasta.heroku.com/edits/new" target="' + ids.iframe + '"><input type="hidden" name="view" value="framed"></form>';
-  blank_dialog = '<div id="' + ids.dialog + '"><iframe frameborder="no" style="margin: 0px; padding: 0px; width: 400px; height: 400px;" id="' + ids.iframe + '" scrolling="no"></iframe></div>';
+  blank_dialog = '<div id="' + ids.dialog + '"><iframe frameborder="no" style="margin: 0px; padding: 0px; width: 400px; height: 400px;" id="' + ids.iframe + '" scrolling="no"></iframe><input type="button" class="close" id="copy-pasta-cancel" style="display:none;"></div>';
   indicator = function() {
     if ($(paths.indicator).length === 0) {
       $('body').append('<div id="' + ids.indicator + '"><p>click to correct</p></div>');
@@ -69,7 +84,13 @@
     return $(paths.form).submit();
   };
   iframe_action = function(e) {
-    return console.debug(e);
+    if (!(e.origin = 'http://localhost:3000')) {
+      return;
+    }
+    if (e.data === 'finished') {
+      $(paths.btn + '.on').click();
+      return dialog().find(paths.cancel_btn).click();
+    }
   };
   _ref = ['p', 'li', 'h1', 'h2', 'h3', 'h4', 'h5'];
   for (_i = 0, _len = _ref.length; _i < _len; _i++) {
