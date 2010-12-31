6d6cefc2a0fac518cfb82468a32fc5dd361fb945
mrkurt <kurt@mubble.net>
2010-12-30 18:57:23 -0600

    Updating compass to work with heroku

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,7 @@
 require 'compass'
 require 'compass/app_integration/rails'
 Compass::AppIntegration::Rails.initialize!
+
+require "fileutils"
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
+Rails.application.config.middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets/compiled"])
