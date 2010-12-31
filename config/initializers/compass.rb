a4bdd36bb1b159a9303a2cc3e8c60de3848afd1e
mrkurt <kurt@mubble.net>
2010-12-30 19:03:50 -0600

    Less stupid tweaks for sass on heroku

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,7 +1,3 @@
 require 'compass'
 require 'compass/app_integration/rails'
 Compass::AppIntegration::Rails.initialize!
-
-require "fileutils"
-FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
-Rails.application.config.middleware.use(Rack::Static, :root => "tmp/", :urls => ["/stylesheets/compiled"])
