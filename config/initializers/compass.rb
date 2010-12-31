12fbe77a10963147c148efd66ae193c940a64664
mrkurt <kurt@mubble.net>
2010-12-30 19:13:07 -0600

    Still trying to get compass working on heroku

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,6 @@
 require 'compass'
 require 'compass/app_integration/rails'
 Compass::AppIntegration::Rails.initialize!
+
+require "fileutils"
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "compiled"))
