00ffaf751b1c4ef2a0fc5846b1f2d679d185b5df
mrkurt <kurt@mubble.net>
2011-01-08 20:22:02 -0600

    Removing mail log messages

--- app/models/edit_observer.rb
+++ app/models/edit_observer.rb
@@ -1,7 +1,6 @@
 class EditObserver < ActiveRecord::Observer
   def after_create(edit)
     Editor.where(:host => edit.page.host).each do |editor|
-      edit.logger.info "Edit created, notifying #{editor.email}"
       EditorMailer.new_edit_notice(edit, editor).deliver
     end
   end
