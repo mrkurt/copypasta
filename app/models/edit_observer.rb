95345b8dc87a0c0b6daea2318254581617723be2
mrkurt <kurt@mubble.net>
2011-01-21 09:01:02 -0600

    Calling deliver on user emails

--- app/models/edit_observer.rb
+++ app/models/edit_observer.rb
@@ -8,12 +8,7 @@ class EditObserver < ActiveRecord::Observer
   def after_update(edit)
     changes = edit.changes
     if changes['status']
-      UserMailer.edit_status_change_notice(edit) unless edit.email.blank?
-      if edit.email.blank?
-        puts "Edit #{edit.id} had no email"
-      end
-    else
-      puts "No changes in status, skipping email"
+      UserMailer.edit_status_change_notice(edit).deliver unless edit.email.blank?
     end
   end
 end
