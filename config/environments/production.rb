6b35d6d544a485813a29f2e5a3e14c7112ecd90b
mrkurt <kurt@mubble.net>
2011-01-27 09:02:19 -0600

    Forcing SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 CopyPasta::Application.configure do
+  config.middleware.use ForceSSL
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
