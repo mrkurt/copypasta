026fffe709ef2b217967245dffb59158a4e39f9d
mrkurt <kurt@mubble.net>
2011-02-14 09:15:18 -0600

    Adding user agent exception to figure out cloudfront

--- app/controllers/embeds_controller.rb
+++ app/controllers/embeds_controller.rb
@@ -1,5 +1,8 @@
 class EmbedsController < ApplicationController
   layout 'copypasta'
+  def index
+    raise request.env['HTTP_USER_AGENT']
+  end
   def show
     @account = Account.find_with_obfuscated_id(params[:id])
   end
